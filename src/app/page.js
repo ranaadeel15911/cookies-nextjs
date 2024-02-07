bf7d6ac96685e71d9ad396b5368c0dbf4fa10da1
ranaadeel15911 <ra0511083@gmail.com>
2024-02-07 10:32:25 +0500

    afja;

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,11 @@
 'use client'
-
 import axios from "axios";
-
-// import { setCookie, getCookie, getCookies, deleteCookie, hasCookie } from 'cookies-next';
-// import { cookies } from 'next/headers';
 const Home =  () => {
-  // It's not possible to update the cookie in RSC
-  //  setCookie("test", "value", { cookies });
-  //  deleteCookie('test1', { cookies });
-  // cookies().set('name', 'value', { maxAge: 0 })
-// console.log(cook)
 const handleGet = async()=>{
   console.log('called')
   try {
     const res = await axios.get('/abc')
     console.log(res)
-    // router.push('/login')
     alert("Got Successful")
   } catch (error) {
     console.log(error)
@@ -26,7 +16,6 @@ const DeleteCookie = async()=>{
   try{
   const res = await axios.get('/def')
   console.log(res)
-  // router.push('/login')
   alert("Deleted")
 } catch (error) {
   console.log(error)
